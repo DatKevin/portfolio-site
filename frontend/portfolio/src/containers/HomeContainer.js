6f332fc86cf7ec31489912def34a3b16d8dcd9ca
Kevin Dat Nguyen <datkevin@gmail.com>
2020-03-07 23:53:18 -0800

    light cs and added Apple to Resume

--- frontend/portfolio/src/containers/HomeContainer.js
+++ frontend/portfolio/src/containers/HomeContainer.js
@@ -1,12 +1,14 @@
 import React from "react"
 import HomeBoxContainer from "./HomeBoxContainer"
 import Bio from "../components/AboutMe/Bio"
+import Apple from "../components/Resume/Apple"
 
 const Home = () => {
 	return(
 		<div className = "container">
 			<HomeBoxContainer />
 			<Bio />
+			<Apple />
 		</div>
 	)
 }
