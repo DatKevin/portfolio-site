b104f53b36709be0e941392e9f8c1025dfe9379c
Kevin Dat Nguyen <datkevin@gmail.com>
2020-03-12 22:20:02 -0700

    light text added

--- frontend/src/components/Resume/SiliconValleyBank/SiliconValleyBank.js
+++ frontend/src/components/Resume/SiliconValleyBank/SiliconValleyBank.js
@@ -4,7 +4,7 @@ class SiliconValleyBank extends Component {
 	render() {
 		return(
 			<div> 
-				<h2>Silicon Valley Bank</h2>
+				<h2>Silicon Valley Bank Resume Summary</h2>
 				<p>
 					Analyze financial information to identify risks, trends, and
 					signs of potential wary activity of clients
@@ -25,6 +25,19 @@ class SiliconValleyBank extends Component {
 					Develop and implement a streamlined process of conducting
 					reviews resulting in reducing the standard one review per hour to
 					one review per twenty minutes while maintaining quality				
+				</p>
+				<h2>Extra Details</h2>
+				<p>
+					My first job was at Silicon Valley Bank as a Enhance Due Diligence(EDD)/Know Your Client(KYC) Analyst. I was hired as a contractor to help with a large backlog of 10,000 reviews for new/existing clients. My days consisted of doing reviews and analysis on incoming bank clients. I would assess their financial records and documents to verify their identity. From there, I would then assign a risk rating and create a 1-2 page report based on EDD/KYC compliance laws and other factors such as location and beneficial owners. 
+				</p>
+				<p>
+					I was a contractor there and we were to work 8 hours a day, 5 days a week. One review took an average of an hour or longer based on the client being reviewed. We were usually assigned 8 reviews a day with the expectation to finish one an hour. 
+				</p>
+				<p>
+					I learned pretty quickly that the reviews were standardized and had certain expectations of what information was presented, which varied based on the client. Because they were standardized, I realized that I can speed up the process by using a template with interchangeable variables to write consistent yet accurate reports. It took me a couple of weeks but I managed to make an easy to use template review and a workflow that could be used to finish a review within 15 to 20 minutes while meeting the same expectations of a high quality review done within an hour. 
+				</p>
+				<p>
+					I shared my template around and several of my coworkers used it to the same degree of success I had. However, there were some coworkers who refused to use it and had their own ways of doing the review. Management was quite impressed but didn’t feel the need to enforce onto the entire team. I finished my contract as the most efficient worker and we finished the 10,000 review backlog 3 months ahead of schedule. 
 				</p>
 			</div>
 		)	
